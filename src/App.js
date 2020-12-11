824cb384e556fc01711a7735d3c52f90ea2e7328
Ariel <Ariel@Maddies-MacBook-Pro.local>
2020-12-11 15:18:31 -0500

    [fetch] used github api to render page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [object, setObject] = useState({});
+
+  //fetch some github data
+  fetch('https://api.github.com/users/facebook')
+  .then(result => {
+    return result.json();
+  })
+  .then(data => {
+    //destructuring
+    const { bio, location, name, login } = data;
+    
+
+    //pass the data through setObject
+    setObject({bio, location, name, login})
+    
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Github Profile</h1>
+      <h2> Name: {object.name} </h2>
+      <h2> Location: {object.location} </h2>
+      <h2> Login: {object.login} </h2> 
+      <h2> Bio: {object.bio} </h2>
+
     </div>
   );
 }
